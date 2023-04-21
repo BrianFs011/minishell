c31ab36b0286369745bf4794d6c30f9de6d79f84
sathyagimenes <sathya.gimenes@gmail.com>
2023-04-21 14:17:35 -0300

    fix output command not found

--- src/tml_functions_utils.c
+++ src/tml_functions_utils.c
@@ -6,7 +6,7 @@
 /*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:21:41 by briferre          #+#    #+#             */
-/*   Updated: 2023/04/18 20:49:30 by sde-cama         ###   ########.fr       */
+/*   Updated: 2023/04/21 14:17:03 by sde-cama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,8 +67,7 @@ int	tml_find_exec(t_ml *tml)
 	if (check)
 	{
 		// printf("minishell: %s: command not found\n", tml->split_cmd[0]);
-		perror(ft_strcat(tml->split_cmd[0], ": command not found\n", FALSE, FALSE));
-		exit(127);
+		ft_putendl_fd("minishell: command not found", STDERR_FILENO);
 	}
 	return (check);
 }
