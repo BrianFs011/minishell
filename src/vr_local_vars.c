ee0d1cc639a55124ff44952270002fad83833f80
sathyagimenes <sathya.gimenes@gmail.com>
2023-05-01 11:12:11 -0300

    fixed memory leak when invalid local var

--- src/vr_local_vars.c
+++ src/vr_local_vars.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vr_local_vars.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: briferre <briferre@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 08:49:39 by briferre          #+#    #+#             */
-/*   Updated: 2023/04/18 20:54:32 by briferre         ###   ########.fr       */
+/*   Updated: 2023/05/01 11:10:02 by sde-cama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ int	vr_new_assignment(t_ml *tml)
 
 	var = vr_get_name_value(tml->split_cmd[0]);
 	if (!ft_strisalpha(var.name))
+	{
+		free(var.name);
+		free(var.value);
 		return (127);
+	}
 	if (!vr_change_value(&(tml->env_vars), var))
 	{
 		if (!vr_change_value(&(tml->local_vars), var))
