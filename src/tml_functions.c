6d6c68a5db2e42b8cc611c4af0cd6c0550b34be0
briferre <briferre@student.42sp.org.br>
2023-05-02 17:02:01 -0400

    remoção do perror do execv

--- src/tml_functions.c
+++ src/tml_functions.c
@@ -6,7 +6,7 @@
 /*   By: briferre <briferre@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 14:14:10 by briferre          #+#    #+#             */
-/*   Updated: 2023/05/02 13:46:25 by briferre         ###   ########.fr       */
+/*   Updated: 2023/05/02 16:47:49 by briferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,9 +83,10 @@ int	tml_exec_child(t_ml *tml, int *fd)
 
 	exit_status = 0;
 	exit_status = rd_redirection(tml, fd);
-	if (((!ft_strncmp(tml->split_cmd[0], "./", 2))
-			|| tml->split_cmd[0][0] == '/'))
+
+	if (!ft_strncmp(tml->split_cmd[0], "./", 2) || tml->split_cmd[0][0] == '/')
 		exit_status = tml_check_access(tml);
+
 	if (condition_for_find_exec(tml) && exit_status == 0)
 		exit_status = tml_find_exec(tml);
 	if (tml->pp_quant != 0 && exit_status == 0 && tml->redirect_out != 1)
@@ -106,7 +107,7 @@ int	tml_exec_child(t_ml *tml, int *fd)
 	env_updated = tml_construct_env(tml);
 	if (execve(tml->split_cmd[0], tml->split_cmd, env_updated) == -1)
 	{
-		perror("Execve");
+		// perror("Execve");
 		tml_free_sprt_cmd(env_updated);
 		tml->running = EXIT_SILENCED;
 		exit_status = 1;
