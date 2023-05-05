15d3104adcb635f768ddb750c49379bd989a4dd4
briferre <briferre@student.42sp.org.br>
2023-05-05 17:02:43 -0400

    fix bt_exit closing with two or more args

--- src/bt_exit.c
+++ src/bt_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bt_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: briferre <briferre@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 08:50:23 by briferre          #+#    #+#             */
-/*   Updated: 2023/04/30 17:50:44 by sde-cama         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:49:38 by briferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,17 @@ int	bt_exit(t_ml *tml)
 	int	exit_status;
 
 	exit_status = 0;
-	if (!ft_strcmp(tml->split_cmd[0], "exit"))
+	if (tml->split_cmd[0] && !tml->split_cmd[1])
 		tml->running = FALSE;
-	if (!tml->split_cmd[1])
-		return (exit_status);
 	else if (tml->split_cmd[1] && !tml->split_cmd[2])
 	{
 		if (ft_foreach(tml->split_cmd[1], ft_isdigit_plus) == -1)
 		{
 			if (ck_isllongint(tml->split_cmd[1]))
+			{
 				exit_status = ft_atoi_llint(tml->split_cmd[1]) % 256;
+				tml->running = FALSE;
+			}
 			else
 				exit_status = exit_error(tml->split_cmd[0],
 						": numeric argument required", 2);
